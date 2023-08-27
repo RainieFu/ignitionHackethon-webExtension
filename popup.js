bc49408144dbe0543e35c395eedf966bb6f3379f
RainieFu <127159957+RainieFu@users.noreply.github.com>
2023-08-26 18:20:33 -0700

    Added new feature
    - will display the converting record on the bottom of the screen in the format of SourceCurrency 1 -> ConvertedAmount TargetCurrency
    - NOTE: the save button isn't working :( and this is not what exactly I want, Im on it

--- popup.js
+++ popup.js
@@ -1,11 +1,19 @@
   document.addEventListener("DOMContentLoaded", function () {
     const convertButton = document.getElementById("convert");
-  
+    const saveButton = document.getElementById("save")
+    const savedPairsContainer = document.getElementById("saved_pairs");
+    const savedPairs = JSON.parse(localStorage.getItem("savedPairs")) || [];
+    const historicalRecords = [];
+
+    displaySavedPairs();
+
+
     convertButton.addEventListener("click", function () {
       const selectedCurrencyFrom = document.getElementById("options").value;
       const selectedCurrencyTo = document.getElementById("options2").value;
       const enterAmount = parseFloat(document.getElementById("enter_amount").value);
        
+      let convertedAmount = 0;
       // Validate the user input
         if (!isValidFloat(enterAmount)) {
             alert("Please enter a valid numeric amount.");
@@ -19,18 +27,57 @@
         .then((response) => response.json())
         .then((data) => {
           if (data.result === "success") {
-            const convertedAmount = data.conversion_result.toFixed(2);
+            convertedAmount = data.conversion_result.toFixed(2);
             document.getElementById("converted").value = convertedAmount;
           } else {
             console.error("Error converting amount");
           }
+
+          // Save historical record
+        const record = {
+            source: selectedCurrencyFrom,
+            amount: convertedAmount/enterAmount, 
+            target: selectedCurrencyTo
+        };
+        historicalRecords.push(record);
+        displaySavedPairs();
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
         });
 
-    function isValidFloat(input) {
-        return /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/.test(input);
+        function isValidFloat(input) {
+            return /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/.test(input);
+            }
+
+        
+
+    });
+
+    saveButton.addEventListener("click", function () {
+        // Save the current pairs to savedPairs
+        const pair = `${selectedCurrencyFrom}-${selectedCurrencyTo}`;
+        if (!savedPairs.includes(pair) && savedPairs.length < 3) {
+          savedPairs.push(pair);
+          localStorage.setItem("savedPairs", JSON.stringify(savedPairs));
+          displaySavedPairs();
         }
     });
+
+    function displaySavedPairs() {
+        savedPairsContainer.innerHTML = "";
+    
+        savedPairs.forEach(pair => {
+          const pairElement = document.createElement("div");
+          pairElement.textContent = pair;
+          savedPairsContainer.appendChild(pairElement);
+        });
+    
+        // Display historical records
+        historicalRecords.slice(0, 3).forEach(record => {
+          const recordElement = document.createElement("div");
+          recordElement.textContent = `${record.source} 1 -> ${record.amount} ${record.target}`;
+          savedPairsContainer.appendChild(recordElement);
+        });
+      }
   });
