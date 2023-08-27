ca0046eae61fb0cc922b358e5a748c3f28a5e6fb
RainieFu <127159957+RainieFu@users.noreply.github.com>
2023-08-26 17:24:34 -0700

    Added error handling
    to avoid user accidentally input non-numeric values

--- popup.js
+++ popup.js
@@ -5,7 +5,13 @@
       const selectedCurrencyFrom = document.getElementById("options").value;
       const selectedCurrencyTo = document.getElementById("options2").value;
       const enterAmount = parseFloat(document.getElementById("enter_amount").value);
-  
+       
+      // Validate the user input
+        if (!isValidFloat(enterAmount)) {
+            alert("Please enter a valid numeric amount.");
+            return; // Exit early if input is not valid
+        }
+        
       const apiKey = "1138a964ad30d631b12eb75e";
       const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/pair/${selectedCurrencyFrom}/${selectedCurrencyTo}/${enterAmount}`;
   
@@ -22,5 +28,9 @@
         .catch((error) => {
           console.error("Error fetching data:", error);
         });
+
+    function isValidFloat(input) {
+        return /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/.test(input);
+        }
     });
   });
